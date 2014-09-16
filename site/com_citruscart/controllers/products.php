1c7ea61c824d2894d0511bb2ee469c08ac32e8e2
Kanin Sringarm <kaninzeed@gmail.com>
2014-09-16 16:31:01 +0700

    Revert "fix bug change currency"
    This reverts commit 7471b72a50a324183f58ba5cfb0492ea1c3c5266.

--- site/com_citruscart/controllers/products.php
+++ site/com_citruscart/controllers/products.php
@@ -2521,6 +2521,34 @@ class CitruscartControllerProducts extends CitruscartController
         $app->redirect($url,$response->html);
         
     }  
+	
+    /*
+     * Set the currency to a different value
+     */
+    function setCurrency()
+    {
+        $currency_id = JRequest::getVar('currency_id', 0);
+        
+        if($currency_id)
+        {
+            $helper = Citruscart::getClass('CitruscartHelperBase', 'helpers._base');
+            $helper->setSessionVariable( 'currency_id', $currency_id );
+        }
+        
+        $return = JRequest::getVar( 'return', '' );
+        if( $return )
+        {
+            $url = base64_decode($return);
+        }
+        else
+        {
+            $url = 'index.php?option=com_citruscart&view=products';
+        }
+        
+        $this->setRedirect(JRoute::_($url));
+        $this->redirect();
+        return;
+    }
    
 }
 
